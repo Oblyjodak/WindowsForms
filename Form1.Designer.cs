df48c4c2f399727637ef1681491144608a7cbcce
Jakub <92061403+Oblyjodak@users.noreply.github.com>
2022-11-17 13:05:34 +0100

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,274 +1,39 @@
-﻿
-namespace WindowsFormsApp2
-{
-    partial class Form1
-    {
-        /// <summary>
-        /// Wymagana zmienna projektanta.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+import React, {useState,useRef,useEffect} from "react"
 
-        /// <summary>
-        /// Wyczyść wszystkie używane zasoby.
-        /// </summary>
-        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+import axios from "axios"
 
-        #region Kod generowany przez Projektanta formularzy systemu Windows
+import './App.css';
 
-        /// <summary>
-        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
-        /// jej zawartości w edytorze kodu.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.imie = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.nazwisko = new System.Windows.Forms.TextBox();
-            this.data = new System.Windows.Forms.DateTimePicker();
-            this.miejsceurodzenia = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.adres = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.telefon = new System.Windows.Forms.MaskedTextBox();
-            this.kodpocztowy = new System.Windows.Forms.MaskedTextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.submitt = new System.Windows.Forms.Button();
-            this.results = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // imie
-            // 
-            this.imie.Location = new System.Drawing.Point(127, 42);
-            this.imie.Name = "imie";
-            this.imie.Size = new System.Drawing.Size(100, 20);
-            this.imie.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(95, 45);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Imię";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 71);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Nazwisko";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 100);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Data urodzenia";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 124);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(92, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Miejsce urodzenia";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(87, 150);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(34, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Adres";
-            // 
-            // nazwisko
-            // 
-            this.nazwisko.Location = new System.Drawing.Point(127, 68);
-            this.nazwisko.Name = "nazwisko";
-            this.nazwisko.Size = new System.Drawing.Size(100, 20);
-            this.nazwisko.TabIndex = 6;
-            // 
-            // data
-            // 
-            this.data.Location = new System.Drawing.Point(127, 94);
-            this.data.Name = "data";
-            this.data.Size = new System.Drawing.Size(200, 20);
-            this.data.TabIndex = 7;
-            // 
-            // miejsceurodzenia
-            // 
-            this.miejsceurodzenia.Location = new System.Drawing.Point(127, 121);
-            this.miejsceurodzenia.Name = "miejsceurodzenia";
-            this.miejsceurodzenia.Size = new System.Drawing.Size(100, 20);
-            this.miejsceurodzenia.TabIndex = 8;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(127, 200);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 126);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 9;
-            this.pictureBox1.TabStop = false;
-            // 
-            // adres
-            // 
-            this.adres.Location = new System.Drawing.Point(127, 147);
-            this.adres.Name = "adres";
-            this.adres.Size = new System.Drawing.Size(100, 20);
-            this.adres.TabIndex = 10;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(0, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "label6";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(79, 200);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(42, 13);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Zdjęcie";
-            // 
-            // telefon
-            // 
-            this.telefon.Location = new System.Drawing.Point(127, 341);
-            this.telefon.Mask = "000-000-000";
-            this.telefon.Name = "telefon";
-            this.telefon.Size = new System.Drawing.Size(100, 20);
-            this.telefon.TabIndex = 13;
-            this.telefon.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
-            // 
-            // kodpocztowy
-            // 
-            this.kodpocztowy.Location = new System.Drawing.Point(127, 174);
-            this.kodpocztowy.Mask = "00-000";
-            this.kodpocztowy.Name = "kodpocztowy";
-            this.kodpocztowy.Size = new System.Drawing.Size(100, 20);
-            this.kodpocztowy.TabIndex = 14;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(47, 177);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(74, 13);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Kod pocztowy";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(79, 344);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(43, 13);
-            this.label9.TabIndex = 16;
-            this.label9.Text = "Telefon";
-            // 
-            // submitt
-            // 
-            this.submitt.Location = new System.Drawing.Point(82, 367);
-            this.submitt.Name = "submitt";
-            this.submitt.Size = new System.Drawing.Size(206, 71);
-            this.submitt.TabIndex = 17;
-            this.submitt.Text = "button1";
-            this.submitt.UseVisualStyleBackColor = true;
-            this.submitt.Click += new System.EventHandler(this.Submitt_Click);
-            // 
-            // results
-            // 
-            this.results.AutoSize = true;
-            this.results.Location = new System.Drawing.Point(476, 71);
-            this.results.Name = "results";
-            this.results.Size = new System.Drawing.Size(41, 13);
-            this.results.TabIndex = 18;
-            this.results.Text = "label10";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.results);
-            this.Controls.Add(this.submitt);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.kodpocztowy);
-            this.Controls.Add(this.telefon);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.adres);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.miejsceurodzenia);
-            this.Controls.Add(this.data);
-            this.Controls.Add(this.nazwisko);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.imie);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+function App() {
+  const [nipInfo, setNipInfo] = useState([])
+  const [theNip, setTheNip] = useState();
+  const date = new Date().toISOString();
+  const [currentDate, setCurrentDate] = useState(date.slice(0,10));
+  const [isDone, setIsDone] = useState(false)
+  const inputRef = useRef();
 
-        }
 
-        #endregion
+  const handleButton = () => {
+    setIsDone(!isDone)
+    if(theNip && currentDate) {
+      axios(`https://wl-api.mf.gov.pl/api/search/nip/${theNip}?date=${currentDate}`)
+    .then((data)=>{
+      setNipInfo(data.data?.result.subject);
+    })
+    .catch((error)=>{
+      console.log(error)
+    })}
+  }
 
-        private System.Windows.Forms.TextBox imie;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox nazwisko;
-        private System.Windows.Forms.DateTimePicker data;
-        private System.Windows.Forms.TextBox miejsceurodzenia;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox adres;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.MaskedTextBox telefon;
-        private System.Windows.Forms.MaskedTextBox kodpocztowy;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Button submitt;
-        private System.Windows.Forms.Label results;
-    }
+  return (
+    <>
+      <input type="text" ref={inputRef} onChange={()=> setTheNip(inputRef.current.value)}/>
+      <button onClick={handleButton}>Klik</button>
+      {
+        isDone ? (<h6>{nipInfo.name}</h6>) : (<h1>Waiting...</h1>)
+      }
+    </>
+  );
 }
 
+export default App;
